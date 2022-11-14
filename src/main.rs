bdaa4f96adc27c11f930142de02b354890a3a98d
Tim Steeman <tmdev82@gmail.com>
2022-11-14 22:42:51 +0100

    add sqlite_schema query to view all tables in database

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
+use sqlx::{migrate::MigrateDatabase, Row, Sqlite, SqlitePool};
 
 const DB_URL: &str = "sqlite://sqlite.db";
 
@@ -18,4 +18,18 @@ async fn main() {
 
     let result = sqlx::query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(250) NOT NULL);").execute(&db).await.unwrap();
     println!("Create user table result: {:?}", result);
+
+    let result = sqlx::query(
+        "SELECT name
+         FROM sqlite_schema
+         WHERE type ='table' 
+         AND name NOT LIKE 'sqlite_%';",
+    )
+    .fetch_all(&db)
+    .await
+    .unwrap();
+
+    for (idx, row) in result.iter().enumerate() {
+        println!("[{}]: {:?}", idx, row.get::<String, &str>("name"));
+    }
 }
