6d066fad7ee09de581e23588d5307030344f9f9b
Tim Steeman <tmdev82@gmail.com>
2022-11-15 21:36:33 +0100

    add insert user and query users and deserialize to struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use sqlx::{migrate::MigrateDatabase, Row, Sqlite, SqlitePool};
+use sqlx::{migrate::MigrateDatabase, FromRow, Row, Sqlite, SqlitePool};
 
 const DB_URL: &str = "sqlite://sqlite.db";
+
+#[derive(Clone, FromRow, Debug)]
+struct User {
+    id: i64,
+    name: String,
+}
 
 #[tokio::main]
 async fn main() {
@@ -31,5 +37,21 @@ async fn main() {
 
     for (idx, row) in result.iter().enumerate() {
         println!("[{}]: {:?}", idx, row.get::<String, &str>("name"));
+    }
+
+    let result = sqlx::query("INSERT INTO users (name) VALUES ('billy')")
+        .execute(&db)
+        .await
+        .unwrap();
+
+    println!("Query result: {:?}", result);
+
+    let user_results = sqlx::query_as::<_, User>("SELECT id, name FROM users")
+        .fetch_all(&db)
+        .await
+        .unwrap();
+
+    for user in user_results {
+        println!("[{}] name: {}", user.id, &user.name);
     }
 }
